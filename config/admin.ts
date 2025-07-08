f9695cbf3fb259e2b6a3d5ba2a0089943b1f5b47
Nassim-dev <nassim.yazi@eiffage.com>
2025-07-08 16:05:33 +0200

    feat: medias

--- config/admin.ts
+++ config/admin.ts
@@ -1,9 +1,9 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'yourRandomAdminSecretHere'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'yourRandomSaltHere'),
   },
   transfer: {
     token: {
